80ddd7ffac2cd9ee09a2dd2061eefc0fbf6eb05f
Chandra Prakash <106953694+MoooN-LighT@users.noreply.github.com>
2023-10-17 16:20:42 +0530

    Initial Project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Exp Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
